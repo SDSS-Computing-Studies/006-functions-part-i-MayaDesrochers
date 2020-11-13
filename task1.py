d55b52d8aa4f5ba68eb32c8047e643dc342c9e0a
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-11-13 10:47:54 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,30 +9,10 @@ def sum(a,b):
     # a : float 
     # b : float
     # return value: returns the sum of the 2 numbers
-<<<<<<< HEAD
-
-#this should return a value of 7
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-    x = sum(a,b)
-    return x
 
 
 #this should return a value of 7
-   
 
 #this should return a value of 12.5
     
->>>>>>> cd721c06ea66fc9b389806f8eaef0e73d580d90d
+
